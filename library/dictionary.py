5c7b24d5b75b4cda3167017c89a1eb6c002202a4
Jamal135 <50566368+Jamal135@users.noreply.github.com>
2022-11-15 00:38:49 +1000

    Improvements

--- library/dictionary.py
+++ library/dictionary.py
@@ -9,25 +9,22 @@ import json
 
 class Symbols:
     ''' Purpose: Creates object for all JSON pixel letters. '''
-    def __init__(self, data):
-        chars = data.keys()
-        for key in chars:
-            _pixels = data[key]
-            setattr(self, key, _pixels)
-        self.allowed = set(chars)
-    def get_coords(self, key, loc: dict = {'x': 1, 'y': 1}):
+    def __init__(self, data: json):
+        self.data = dict(data)
+        self.allowed = set(data.keys())
+    def get_coords(self, key: str, loc: list):
         ''' Returns: Width plus x, and y coordinates for key. '''
-        pixels = getattr(self, key)
+        pixels = self.data[key]
         x, y = [], []
         for y_index, array in enumerate(pixels[::-1]):
             for x_index, item in enumerate(array):
                 if item != 0:
-                    x.append(loc['x'] + x_index)
-                    y.append(loc['y'] + y_index)
+                    x.append(loc[0] + x_index)
+                    y.append(loc[1] + y_index)
         return {'width': len(pixels[0]), 'x': x, 'y': y}
 
 
 data = json.load(open("library/slumbers.json"))
 x = Symbols(data)
-print(x.get_coords('K', {'x': 1, 'y': 1}))
-print(x.get_coords('A', {'x': 5, 'y': 1}))
+print(x.get_coords('K', [1,1]))
+print(x.get_coords('A', [5,1]))
